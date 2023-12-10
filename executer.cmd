13332514e80b71c5a9e196cab88248a52d7f4ba7
Wol_loW <gioele.pettarini@gmail.com>
2023-12-10 23:16:24 +0100

    'cd' handles path correctly. 'z' not yet. Accented letters are not handled correctly.

--- executer.cmd
+++ executer.cmd
@@ -1,12 +1,12 @@
 @ECHO OFF
 
 IF "%3"=="-cd" (
-    %1 %2 -a %~dpfn4
+    %1 %2 -a "%~dpfn4"
     IF NOT ERRORLEVEL 1 ( EXIT /B 0 ) ELSE ( EXIT /B 1 )
 )
 
 IF "%3"=="-x" (
-    %1 %2 -x %~dpfn4
+    %1 %2 -x "%~dpfn4"
     IF NOT ERRORLEVEL 1 ( EXIT /B 0 ) ELSE ( EXIT /B 1 )
 )
 
