0c0f9f9faef2fb5d5331caccd91118d9254f6db5
GiGGioSo <gioele.pettarini@gmail.com>
2024-01-17 17:30:36 +0100

    Corrected handling of paths with double quotes.

--- _jump_dirs_executer.cmd
+++ _jump_dirs_executer.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 
-IF "%4"=="-cd" (
-    IF "%5"=="/d" (
+IF "%~4"=="-cd" (
+    IF "%~5"=="/d" (
         %1 %2 %3 -a "%~dpfn6\"
         IF NOT ERRORLEVEL 1 ( EXIT /B 0 ) ELSE ( EXIT /B 1 )
     )
@@ -9,12 +9,12 @@ IF "%4"=="-cd" (
     IF NOT ERRORLEVEL 1 ( EXIT /B 0 ) ELSE ( EXIT /B 1 )
 )
 
-IF "%4"=="-x" (
+IF "%~4"=="-x" (
     %1 %2 %3 -x "%~dpfn5\"
     IF NOT ERRORLEVEL 1 ( EXIT /B 0 ) ELSE ( EXIT /B 1 )
 )
 
-IF "%4"=="-c" (
+IF "%~4"=="-c" (
     %1 %2 %3 %CD% %5 %6 %7 %8 %9
     IF NOT ERRORLEVEL 1 ( EXIT /B 0 ) ELSE ( EXIT /B 1 )
 )
